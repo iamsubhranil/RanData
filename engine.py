c85d6736646e28e70acc731a3e836e02d0546b67
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2019-11-24 00:56:33 +0530

    updt: engine: more vectorization Now, if both the callee and the arguments are constant, the 'times' info is passed straight to the called method with only one argument. The method evalutes appropiately for 'times' iteration, and returns the whole result back to the engine. This greatly helps 'value.one_of' and family of calls.

--- engine.py
+++ engine.py
@@ -18,47 +18,70 @@ class Value:
     def __str__(self):
         return str(self.val)
 
-    def is_str(self):
-        return isinstance(self.val, str)
-
-    def normalize(self):
-        if self.is_str():
-            self.val.replace("'", "''")
-
     def append(self, y):
         ret = str(self.val)
         for i in y:
             ret += str(i)
         return Value(ret)
 
+    def append_times(self, x, y):
+        return [self.append(x)] * y
+
     def constant(self, x):
         return Value(x[0], True)
 
-    def one_of(self, l):
-        idx = random.randint(0, len(l) - 1)
-        return Value(l[idx])
+    def constant_times(self, x, y):
+        return [Value(x[0], True)] * y
 
-    def lower(self, l):
-        return Value(str(self.val).lower())
+    def one_of(self, l):
+        return Value(random.choice(l))
+
+    def one_of_times(self, l, y):
+        res = []
+        for _ in range(y):
+            res.append(Value(random.choice(l)))
+        return res
+
+    def lower(self):
+        return Value(str(self.val).lower(), self.is_constant)
+
+    def lower_times(self, x):
+        return [self.lower()] * x
 
     def one_of_unique(self, l):
         origlist = l
         while len(origlist) == 1 and isinstance(origlist[0], Value):
             origlist = origlist[0].val
         l = origlist
-        tup = tuple(l)
+        tup = frozenset(l)
         if tup in self.UNIQUE_DICTIONARY:
-            if len(self.UNIQUE_DICTIONARY[tup]) == len(tup):
+            dictionary = self.UNIQUE_DICTIONARY[tup]
+            if len(dictionary) == len(tup):
                 raise EngineError("No more unique values to generate!")
-            v = self.one_of(l)
-            while v in self.UNIQUE_DICTIONARY[tup]:
-                v = self.one_of(l)
-            self.UNIQUE_DICTIONARY[tup].append(v)
-            return v
         else:
-            v = self.one_of(l)
-            self.UNIQUE_DICTIONARY[tup] = [v]
-            return v
+            self.UNIQUE_DICTIONARY[tup] = set()
+            dictionary = self.UNIQUE_DICTIONARY[tup]
+
+        v = random.sample(tup - dictionary, 1)
+        dictionary.add(v)
+        return v
+
+    def one_of_unique_times(self, l, number):
+        origlist = l
+        while len(origlist) == 1 and isinstance(origlist[0], Value):
+            origlist = origlist[0].val
+        l = origlist
+        tup = frozenset(l)
+        if tup in self.UNIQUE_DICTIONARY:
+            dictionary = self.UNIQUE_DICTIONARY[tup]
+            if len(dictionary) <= (len(tup) + number):
+                raise EngineError("Required %d unique values cannot be generated!" % number)
+        else:
+            self.UNIQUE_DICTIONARY[tup] = set()
+            dictionary = self.UNIQUE_DICTIONARY[tup]
+        v = random.sample(tup - dictionary, number)
+        dictionary.update(v)
+        return v
 
     def __hash__(self):
         if isinstance(self.val, Value):
@@ -73,11 +96,33 @@ class Value:
 
 class Number(Value):
 
+    def between_times(self, x, y):
+        if len(x) != 2:
+            raise EngineError(
+                "number.between takes two arguments, %d were given" % len(x))
+        res = []
+        lower = int(x[0].val)
+        upper = int(x[1].val)
+        ran = random.randint
+        for _ in range(y):
+            res.append(Value(ran(lower, upper)))
+        return res
+
     def between(self, x):
         if len(x) < 2:
             raise EngineError(
                 "number.between takes two arguments, 1 were given")
         return Value(random.randint(x[0].val, x[1].val))
+
+    def upto_times(self, x, y):
+        if len(x) != 2:
+            raise EngineError("upto takes one argument, %d were given" % len(x))
+        res = []
+        upper = int(x[0].val)
+        ran = random.randint
+        for _ in range(y):
+            res.append(Value(ran(0, upper)))
+        return res
 
     def upto(self, x):
         return Value(random.randint(0, x[0].val))
@@ -128,13 +173,24 @@ class Engine(AstVisitor):
         if not callable(func):
             raise EngineError("Invalid method name '%s'!" % ast.func)
         args = []
+        arg_is_constant = True
         for arg in ast.args:
             temp = self.visit(arg)
+            arg_is_constant = arg_is_constant and temp[0].is_constant
             args.append(temp)
 
         res = []
         argsorted = zip(*args)
         if obj[0].is_constant:
+            if arg_is_constant:
+                bakfunc = func
+                func = getattr(obj[0], str(ast.func.val) + "_times", None)
+                if not callable(func):
+                    print("[Warning] Should've implemented '%s' on '%s'!"
+                          % (str(ast.func.val) + "_times", obj[0].__class__))
+                    func = bakfunc
+                else:
+                    return func(next(argsorted), self.times)
             for a in argsorted:
                 res.append(func(a))
         else:
